788741a1fd196b9b23fc46ef192f9adee85925bc
Jiong Qi <jiongqipan@gmail.com>
2023-03-20 01:20:47 -0400

    Fix shopping cart styling

--- frontend/src/components/LibraryPage/index.js
+++ frontend/src/components/LibraryPage/index.js
@@ -3,27 +3,33 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
 import { fetchLibraryItems } from "../../store/libraryItem";
 import LibraryItem from "./LibraryItem";
+import demoIcon from '../../assets/png-transparent-robotic-process-automation-computer-icons-robotics-roboto-electronics-silhouette-black.png'
 import "./LibraryPage.css"
 
 const LibraryPage = () => {
     const dispatch = useDispatch();
     const currentUser = useSelector(state => state.session.user);
+    if (currentUser) {
+        console.log(currentUser);
+    }
 
     const libraryItemsArray= useSelector(state => state.libraryItems ? Object.values(state.libraryItems) : []);
 
     const libraryItems = libraryItemsArray.map(libraryItem => <LibraryItem libraryItem={libraryItem} key={libraryItem.id} />);
-    console.log(libraryItems);
     useEffect(() => {
         dispatch(fetchLibraryItems());
     }, [dispatch])
-    console.log("library-page")
 
     if (!currentUser) {
         return <Redirect to="login" />;
     } else {
         return (
             <div className="library-page-content">
-                <h1>Library</h1>
+                <div className="library-header">
+                    <img className="library-profile-pic" src={demoIcon} alt=""></img>
+                    <div className="library-username">{currentUser.username}</div>
+
+                </div>
                 {libraryItems}
                 <div>
                     {libraryItemsArray.title}
