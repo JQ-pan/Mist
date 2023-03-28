ab510e5b34ac3d112e24639a5892c7b0d808c6e7
Jiong Qi <jiongqipan@gmail.com>
2023-03-27 22:42:15 -0400

    Style reviews

--- frontend/src/components/Reviews/ReviewItem.js
+++ frontend/src/components/Reviews/ReviewItem.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchUsers } from "../../store/users";
+import defaultProfileIcon from "../../assets/png-transparent-robotic-process-automation-computer-icons-robotics-roboto-electronics-silhouette-black.png"
 import './ReviewItem.css';
 
 const ReviewItem = ({ reviewItem, user }) => {
@@ -10,10 +11,16 @@ const ReviewItem = ({ reviewItem, user }) => {
     //     dispatch(fetchUsers());
     // }, [dispatch])
     // console.log(reviewItem);
+    // console.log({user});
     return (
         <div className="review-item">
             <div className="left-col">
-                {user.username}
+                <div className="profile-icon-container">
+                    <img className="profile-icon"src={defaultProfileIcon} default=""></img>
+                </div>
+                <div className="review-username">
+                    <span>{user.username}</span>
+                </div>
             </div>
 
             <div className="right-col">
