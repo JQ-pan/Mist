e0f08f19e07ad51137fd306a599a72eb8be2ecc5
Jiong Qi <jiongqipan@gmail.com>
2023-04-06 09:56:43 -0400

    Continue seeding images

--- frontend/src/components/GameIndexPage/FeaturedItem.js
+++ frontend/src/components/GameIndexPage/FeaturedItem.js
@@ -3,16 +3,16 @@ import './FeaturedItem.css';
 const FeaturedItem = ({ game }) => {
     return (
         <div className="featured-container">
-            <img className="main-image" src={game.images[0]} alt="" />
+            <img className="main-image" src={game.images[1]} alt="" />
             <div className="featured-info">
                 <div className="featured-game-title">
                     <h3>{game.title}</h3>
                 </div>
                 <div className="featured-image-container">
-                    <div><img className="side-image" src={game.images[1]} alt="" /></div>
                     <div><img className="side-image" src={game.images[2]} alt="" /></div>
                     <div><img className="side-image" src={game.images[3]} alt="" /></div>
                     <div><img className="side-image" src={game.images[4]} alt="" /></div>
+                    <div><img className="side-image" src={game.images[5]} alt="" /></div>
                 </div>
             </div>
 
