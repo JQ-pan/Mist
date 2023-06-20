0bf9883ee05e5ecec7ce401c2fb4fe5c3d167a05
Jiong Qi <jiongqipan@gmail.com>
2023-06-19 23:43:46 -0400

    Refactored index preview tab

--- frontend/src/components/GameIndexPage/GameIndex.js
+++ frontend/src/components/GameIndexPage/GameIndex.js
@@ -10,7 +10,6 @@ import './GameIndex.css';
 const GameIndex = () => {
     const dispatch = useDispatch();
     const games = useSelector(state => Object.values(state.games))
-
     useEffect(() => {
         dispatch(fetchGames());
         dispatch(fetchAllReviews());
@@ -20,7 +19,8 @@ const GameIndex = () => {
     const handleMouseEnter = index => {
         setIndex(index);
     }
-    const gamePreview = games.length > 0 ? games[index] : false
+    const gamePreview = games.length > 0 ? games[index] : false;
+
     const gameItems = games.map((game, i) =>
         <div key={i} onMouseEnter={() => handleMouseEnter(i)} className="{}">
             <GameIndexItem key={i} game={game} />
@@ -63,42 +63,51 @@ const GameIndex = () => {
         reviewObject[key] = [length, averageRating];
     }
 
-    return (
-        <div className="game-display-background">
-            <StoreNavigation />
-            <Featured games={games} />
-            <div className="game-display-container">
-                <div className="game-display-main">
-                    <div className="game-left-display-container">
-                        <ul id="games-list">
-                            {gameItems}
-                        </ul>
-                    </div>
-                    <div className="tab-preview-container">
-                        <div className="tab-preview-content">
-                            {gamePreview && Object.keys(gamePreview).length > 0 && Object.keys(reviewObject).length > 0 && (
-                                <>
-                                    <h2>{gamePreview.title}</h2>
-                                    <div className="tab-preview-summary">
-                                        <div className="tab-preview-title">Overall user reviews:</div>
-                                        <span className="game-review-summary" style={{
-                                            color:
-                                                reviewObject[gamePreview.id][1] === "Negative" ? "#c35c2c" :
-                                                    reviewObject[gamePreview.id][1] === "Mostly Negative" ? "#c35c2c" :
-                                                        reviewObject[gamePreview.id][1] === "Mixed" ? "#a8926a" :
-                                                            reviewObject[gamePreview.id][1] === "Mostly Positive" ? "#66C0F4" : "#66C0F4"
-                                        }}>{reviewObject[gamePreview.id][1]}</span>
-                                        <span>&nbsp;({reviewObject[gamePreview.id][0]})</span>
-                                    </div>
-                                </>
-                            )}
+    if (games.length === 0) {
+        return <>Still loading...</>
+    } else {
+        return (
+            <div className="game-display-background">
+                <StoreNavigation />
+                <Featured games={games} />
+                <div className="game-display-container">
+                    <div className="game-display-main">
+                        <div className="game-left-display-container">
+                            <ul id="games-list">
+                                {gameItems}
+                            </ul>
+                        </div>
+                        <div className="tab-preview-container">
+                            <div className="tab-preview-content">
+                                {gamePreview && Object.keys(gamePreview).length > 0 && Object.keys(reviewObject).length > 0 && (
+                                    <>
+                                        <h2>{gamePreview.title}</h2>
+                                        <div className="tab-preview-summary">
+                                            <div className="tab-preview-title">Overall user reviews:</div>
+                                            <span className="game-review-summary" style={{
+                                                color:
+                                                    reviewObject[gamePreview.id][1] === "Negative" ? "#c35c2c" :
+                                                        reviewObject[gamePreview.id][1] === "Mostly Negative" ? "#c35c2c" :
+                                                            reviewObject[gamePreview.id][1] === "Mixed" ? "#a8926a" :
+                                                                reviewObject[gamePreview.id][1] === "Mostly Positive" ? "#66C0F4" : "#66C0F4"
+                                            }}>{reviewObject[gamePreview.id][1]}</span>
+                                            <span>&nbsp;({reviewObject[gamePreview.id][0]})</span>
+                                        </div>
+                                        <ul className="tab-preview-images">
+                                            <li><img src={gamePreview.images[1]} alt="" /></li>
+                                            <li><img src={gamePreview.images[2]} alt="" /></li>
+                                            <li><img src={gamePreview.images[3]} alt="" /></li>
+                                            <li><img src={gamePreview.images[4]} alt="" /></li>
+                                        </ul>
+                                    </>
+                                )}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-    )
+        )
+    }
 }
 
 export default GameIndex;
