6a3545c694dfc335bd70260aca47bc4aa69b7600
Jiong Qi <jiongqipan@gmail.com>
2023-07-22 12:18:26 -0400

    Refactoring

--- frontend/src/components/BottomBlock/index.js
+++ frontend/src/components/BottomBlock/index.js
@@ -7,47 +7,39 @@ import logo from '../../assets/steam-icon-14885.png'
 function BottomBlock() {
     const sessionUser = useSelector(state => state.session.user);
 
-    let bottomContent;
-
     if (sessionUser) {
-        bottomContent = (
-            <>
-                <div className="footer-content">
-                    <div className="loggedin-footer-content">
-                        <img className="img" src={logo} alt=""></img>
-                        <div className="text1">Keep scrolling for more recommendations</div>
-                        <div className="text2">Below, you'll find a variety of titles that you may be interested in from categories across Mist</div>
-                    </div>
+        return (
+            <div className="footer-content">
+                <div className="loggedin-footer-content">
+                    <img className="img" src={logo} alt=""></img>
+                    <div className="text1">Keep scrolling for more recommendations</div>
+                    <div className="text2">Below, you'll find a variety of titles that you may be interested in from categories across Mist</div>
                 </div>
-            </>
+            </div>
         )
     } else {
-        bottomContent = (
-            <>
-                <div className="footer-content">
+        return (
+            <div className="footer-content">
 
-                    <div className="loggedout-content">
-                        <div className="sign-in-content">
-                            <br />
-                            Sign in to view personalized recommendations
-                            <div className="button-content">
-                                <div>
-                                    <NavLink className="bottom-login-button" to="/login">
-                                        <span>Sign In</span>
-                                    </NavLink>
-                                </div>
-                                <br />
-                                <br />
-                                <div className="bottom-content">Or <NavLink className="bottom-signup-button" to="/signup">sign up</NavLink> and join Mist for free</div>
+                <div className="loggedout-content">
+                    <div className="sign-in-content">
+                        <br />
+                        Sign in to view personalized recommendations
+                        <div className="button-content">
+                            <div>
+                                <NavLink className="bottom-login-button" to="/login">
+                                    <span>Sign In</span>
+                                </NavLink>
                             </div>
+                            <br />
+                            <br />
+                            <div className="bottom-content">Or <NavLink className="bottom-signup-button" to="/signup">sign up</NavLink> and join Mist for free</div>
                         </div>
                     </div>
                 </div>
-            </>
+            </div>
         )
     }
-
-    return bottomContent;
 }
 
 export default BottomBlock;
