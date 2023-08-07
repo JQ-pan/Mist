24f6e61ebc822c6ebd8afad7b561629c6a975a8e
Jiong Qi <jiongqipan@gmail.com>
2023-08-07 14:28:27 -0400

    Add categories page

---
+++ frontend/src/components/GameCategoriesPage/index.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './GameCategoriesPage.css';
+
+const GameCategoriesPage = () => {
+
+}
+
+export default GameCategoriesPage;
