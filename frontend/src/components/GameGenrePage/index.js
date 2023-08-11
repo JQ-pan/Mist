120c2c4ab86a17dee46b6b94ed36c03a7329638a
Jiong Qi <jiongqipan@gmail.com>
2023-08-11 17:18:11 -0400

    Category Formatting Setup

--- frontend/src/components/GameGenrePage/index.js
+++ frontend/src/components/GameGenrePage/index.js
@@ -17,13 +17,32 @@ const GameGenrePage = () => {
         return <>Loading</>
     }
 
-    return games && <div id="main-background" 
-        style={{
-        backgroundImage: `url(${games[0].images[0]})`,
-
-    }}
-        >
-        <StoreNavigation />
+    return games && <div id="page-background">
+        <StoreNavigation id="category-navbar"/>
+        <div id="main-background" 
+            style={{
+                backgroundImage: `url(${games[0].images[0]})`
+            }}
+            
+            >
+        </div>
+        <div className="container" id="testing">
+            <div className="category-section-title">
+                Recommended For You
+            </div>
+            <div className="test">
+                
+            </div>
+        </div>
+        <div className="container">
+            <div className="category-section-title">
+                Popular Titles
+            </div>
+            <div className="test">
+                
+            </div>
+        </div>
+        <div className="footer-spacer" />
     </div>
 }
 
