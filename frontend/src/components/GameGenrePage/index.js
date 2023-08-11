14d2f2252eee5ce5a58147d1c274d1c0f08fc4f9
Jiong Qi <jiongqipan@gmail.com>
2023-08-11 18:38:30 -0400

    Carousel functionality on categories page

--- frontend/src/components/GameGenrePage/index.js
+++ frontend/src/components/GameGenrePage/index.js
@@ -2,18 +2,32 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchGames } from '../../store/game';
+import { fetchAllReviews } from '../../store/review';
+
 import StoreNavigation from '../StoreNavigation';
 import './GameGenrePage.css';
 
 const GameGenrePage = () => {
     const dispatch = useDispatch();
     const { genre } = useParams();
+
     useEffect(() => {
         dispatch(fetchGames());
+        dispatch(fetchAllReviews());
     }, [dispatch, genre])
+
     const games = useSelector(state => Object.values(state.games).filter(game => game.tag.some(tag => tag.toLowerCase().replace(/ /g, '-') === genre)))
 
-    const game = games[0];
+    const [index, setIndex] = useState(0);
+    const game = games[index];
+
+    const increment = () => {
+        index == games.length - 1? setIndex(0) : setIndex(index + 1);
+    }
+
+    const decrement = () => {
+        setIndex(index + 1);
+    }
 
     if (!games.length) {
         return <>Loading</>
@@ -21,23 +35,40 @@ const GameGenrePage = () => {
 
     return games && <div id="page-background">
         <StoreNavigation id="category-navbar"/>
-        <div id="main-background" 
+        <div id="category-background" 
             style={{
-                backgroundImage: `url(${games[0].images[0]})`
+                backgroundImage: `url(${game.images[0]})`
             }}
             >
         </div>
-        <div className="container">
-            <div className="main-carousel-title">{genre}</div>
-            <div className="main-carousel-content">
+        <div className="carousel-container">
+            <div className="category-carousel-title">{genre}</div>
+            <div className="category-carousel-content">
                 <img className="slide-image" src={game.images[0]}/>
-                <div className="featured-info">
-                    <div className="featured-title">
-                        {game.title}
+                <div className="category-featured-content">
+                    <div className="category-featured-info">
+                        <div className="category-featured-title">
+                            {game.title}
+                        </div>
+                        <div className="category-featured-release-date">
+                            {game.releaseDate}
+                        </div>
+                        <div className="category-featured-rating-container">
+                            <div className="category-featured-rating-content">
+                                Very Positive <span> | 231,560 User Reviews </span>
+                            </div>
+                        </div>
                     </div>
-                    <div className="featured-release-date"></div>
-                    <div className="featured-rating"></div>
-                    <div className="featured-tags"></div>
+                    <div className="category-featured-tag-box">
+                        {game.tag.map(tag => <div className="category-featured-tags">{tag}</div>)}
+                    </div>
+                    <div className="category-featured-info-box">
+                        {game.description}
+                    </div>
+                    <div className="buttons">
+                        <button onClick={decrement}>Prev</button>
+                        <button onClick={increment}>Next</button>
+                    </div>
                 </div>
             </div>
         </div>
