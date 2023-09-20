1ead3f16fc268226d274c2acddc1d22a8d249e1e
Jiong Qi <jiongqipan@gmail.com>
2023-09-19 20:52:51 -0400

    Review Utility function

--- frontend/src/utils/reviewUtils.js
+++ frontend/src/utils/reviewUtils.js
@@ -1,7 +1,8 @@
 // This function will take in the game reviews and return an array that looks like [average, color, length]
 // Reviews is an array
-const getAverageReviews = (reviews) => {
-    const output = reviews.map(review => {
+const reviewUtils = (reviews) => {
+    // reviewInfo == []
+    const reviewInfo = reviews.map(review => {
         return [review.gameId, review.recommended]
     }).reduce((acc, [key, value]) => {
         if (!acc[key]) {
@@ -11,3 +12,5 @@ const getAverageReviews = (reviews) => {
         return acc;
     }, {})
 }
+
+export default reviewUtils;
